9c4e120b4530ef02ee62557073d533c31f52c213
secret_online <SecretOnline@users.noreply.github.com>
2015-08-09 11:59:56 +1200

    Add themes

--- clock.js
+++ clock.js
@@ -1,43 +1,46 @@
 (function() {
   function initClock() {
+    changeTheme();
     updateClock();
     updateDate();
   }
 
   function millisUntilMin() {
-    var now = new Date(Date.now());
+    var now = new Date();
     var nextMin = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 1);
     return nextMin.getTime() - Date.now();
   }
 
   function millisUntilDay() {
-    var now = new Date(Date.now());
+    var now = new Date();
     var nextDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
     return nextDay.getTime() - Date.now();
   }
 
   function updateClock() {
     var clock = document.querySelector('.clock');
-    var now = new Date(Date.now());
-
-    console.log(now);
-
+    var now = new Date();
     clock.innerHTML = toTimeString(now);
-
-    var body = document.querySelector('body');
-    if (body.style.backgroundColor != colors[now.getHours()])
-      body.style.backgroundColor = colors[now.getHours()];
+    updateTheme();
 
     setTimeout(updateClock, millisUntilMin());
   }
 
   function updateDate() {
     var date = document.querySelector('.date');
-    var now = new Date(Date.now());
-
+    var now = new Date();
     date.innerHTML = toDateString(now);
 
     setTimeout(updateDate, millisUntilDay());
+  }
+
+  function updateTheme() {
+    var body = document.querySelector('body');
+    var main = document.querySelector('main');
+    var now = new Date();
+    var bgIndex = Math.floor(themes[currentTheme].background.length * (((now.getHours() * 60) + now.getMinutes()) / 1440));
+    body.style.backgroundColor = themes[currentTheme].background[bgIndex];
+    main.style.color = themes[currentTheme].text;
   }
 
   function toTimeString(time) {
@@ -58,33 +61,56 @@
     return days[time.getDay()] + ', ' + time.getDate() + ' ' + months[time.getMonth()];
   }
 
-  // Colors taken from AOSP clock, used in Android
-  var colors = [
-    '#212121',
-    '#27232e',
-    '#2d253a',
-    '#332847',
-    '#382a53',
-    '#3e2c5f',
-    '#442e6c',
-    '#393a7a',
-    '#2e4687',
-    '#235395',
-    '#185fa2',
-    '#0d6baf',
-    '#0277bd',
-    '#0d6cb1',
-    '#1861a6',
-    '#23569b',
-    '#2d4a8f',
-    '#383f84',
-    '#433478',
-    '#3d3169',
-    '#382e5b',
-    '#322b4d',
-    '#2c273e',
-    '#272430'
-  ];
+  function changeTheme(themeName) {
+    if (!(themeName || currentTheme || themes[currentTheme]))
+      themeName = 'aosp';
+
+    currentTheme = themeName;
+    document.cookie = 'theme=' + encodeURIComponent(currentTheme) + ';expires=' + new Date();
+  }
+
+  // Default to Android clock (for now)
+  var currentTheme;
+  // Themes!
+  var themes = {
+    aosp: {
+      name: 'Android Clock',
+      text: '#fff',
+      background: [
+        '#212121',
+        '#27232e',
+        '#2d253a',
+        '#332847',
+        '#382a53',
+        '#3e2c5f',
+        '#442e6c',
+        '#393a7a',
+        '#2e4687',
+        '#235395',
+        '#185fa2',
+        '#0d6baf',
+        '#0277bd',
+        '#0d6cb1',
+        '#1861a6',
+        '#23569b',
+        '#2d4a8f',
+        '#383f84',
+        '#433478',
+        '#3d3169',
+        '#382e5b',
+        '#322b4d',
+        '#2c273e',
+        '#272430'
+      ]
+    },
+    plain: {
+      name: 'Black on White',
+      text: '#000',
+      background: [
+        '#fff'
+      ]
+    }
+  };
   var days = [
     'Sunday',
     'Monday',
