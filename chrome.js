d3ef04c78d5dc806d74af7902ba97896cd988162
secret_online <SecretOnline@users.noreply.github.com>
2015-08-09 10:03:16 +1200

    Add header + change chrome window mode
    Settings button is currently unused. Will have functionality soon
    (hopefully)

--- chrome.js
+++ chrome.js
@@ -1,3 +1,33 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {});
+  chrome.app.window.create('index.html', {
+    frame: 'none'
+  }, function(newWin) {
+    var doc = newWin.contentWindow.document;
+    newWin.contentWindow.addEventListener('load', function() {
+      var headerUl = doc.querySelector('header ul');
+
+      var closeButton = doc.createElement('button');
+      closeButton.type = 'button';
+      closeButton.classList.add('close');
+      closeButton.innerHTML = '<img src="res/close.svg" alt="Close" />';
+      closeButton.addEventListener('click', function() {
+        newWin.close();
+      });
+      var closeLi = doc.createElement('li');
+      closeLi.appendChild(closeButton);
+
+      var minButton = doc.createElement('button');
+      minButton.type = 'button';
+      closeButton.classList.add('minimize');
+      minButton.innerHTML = '<img src="res/minimize.svg" alt="Minimize" />';
+      minButton.addEventListener('click', function() {
+        newWin.minimize();
+      });
+      var minLi = doc.createElement('li');
+      minLi.appendChild(minButton);
+
+      headerUl.appendChild(minLi);
+      headerUl.appendChild(closeLi);
+    });
+  });
 });
