3ce650d13c4eff44c2060abd8acce98d1d0c2bd1
secret_online <me+github@secretonline.co>
2015-09-10 20:37:27 +1200

    Comments!
    I should really put these in as I go. I'm not that great at this. There
    were a few to begin with, but I generally try to lay my code out in a
    nice way to follow.

--- clock.js
+++ clock.js
@@ -1,49 +1,81 @@
 (function() {
+  /**
+   * Perform first-time initialisation of the page
+   */
   function initClock() {
+    // Set theme and clock information
     changeTheme();
     updateClock();
     updateDate();
+
+    // Add click handlers
     document.querySelector('button.themes').addEventListener('click', showThemesDropdown);
   }
 
+  /**
+   * Returns the number of milliseconds until the next minute mark
+   */
   function millisUntilMin() {
     var now = new Date();
     var nextMin = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 1);
     return nextMin.getTime() - Date.now();
   }
 
+  /**
+   * Returns the number of milliseconds until the next day
+   */
   function millisUntilDay() {
     var now = new Date();
     var nextDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
     return nextDay.getTime() - Date.now();
   }
 
+  /**
+   * Updates the time displayed
+   */
   function updateClock() {
+    // Update time
     var clock = document.querySelector('.clock');
     var now = new Date();
     clock.innerHTML = toTimeString(now);
 
+    // Turn on transitions
     var body = document.querySelector('body');
     body.classList.add('transition');
+
+    // Update theme
+    // Make sure theme still exists
     try {
       updateTheme();
     } catch (ex) {
       console.warn('Unable to update theme. Was it deleted?');
     }
+
+    // Remove transition class after 10 seconds
     setTimeout(function() {
       body.classList.remove('transition');
     }, 10000);
+
+    // Set clock to update mext minute
     setTimeout(updateClock, millisUntilMin());
   }
 
+  /**
+   * Chnges the date displayed
+   */
   function updateDate() {
+    // Update date text
     var date = document.querySelector('.date');
     var now = new Date();
     date.innerHTML = toDateString(now);
 
+    // Schedule next update
     setTimeout(updateDate, millisUntilDay());
   }
 
+  /**
+   * Change styles according to the currently selected theme
+   */
   function updateTheme() {
     if (currentTheme) {
       var body = document.querySelector('body');
@@ -53,19 +85,28 @@
     }
   }
 
+  /**
+   * Get the background of the theme at the current time
+   */
   function getThemeBackground(theme, time) {
+    // Set variable defaults
     if (!theme)
       theme = currentTheme;
     if (!time)
       time = Date.now();
 
+    // Get theme object
     var themeObj = themes[theme] || customThemes[theme];
 
+    // Calculate colour
     var now = new Date(time);
     var bgIndex = Math.floor(themeObj.background.length * (((now.getHours() * 60) + now.getMinutes()) / 1440));
     return themeObj.background[bgIndex];
   }
 
+  /**
+   * Gets theme's text colour
+   */
   function getThemeText(theme) {
     if (!theme)
       theme = currentTheme;
@@ -75,6 +116,9 @@
     return themeObj.text;
   }
 
+  /**
+   * Gets theme's font family
+   */
   function getThemeFont(theme) {
     if (!theme)
       theme = currentTheme;
@@ -84,6 +128,9 @@
     return themeObj.font || '';
   }
 
+  /**
+   * Converts Date object to a friendlier time string
+   */
   function toTimeString(time) {
     var hours = time.getHours().toString();
     if (hours > 12)
@@ -98,18 +145,29 @@
     return hours + ':' + minutes + '<sub>' + amPm + '</sub>';
   }
 
+  /**
+   * Converts Date object to frientlier time string
+   */
   function toDateString(time) {
     return days[time.getDay()] + ', ' + time.getDate() + ' ' + months[time.getMonth()];
   }
 
+  /**
+   * Change the theme to be used
+   */
   function changeTheme(themeName) {
+    // Force remove transition class
     document.querySelector('body').classList.remove('transition');
+
     // If theme not set yet, try getting out of storage
     if (!(currentTheme || themeName)) {
       if (chrome.storage) {
+        // Get stored data
         chrome.storage.local.get(['theme', 'customThemes'], function(items) {
+          // If custom themes are stored, add them
           if (items.customThemes)
             customThemes = items.customThemes;
+          // If current theme is stored, set the theme
           if (items.theme)
             changeTheme(items.theme);
           else
@@ -119,41 +177,57 @@
       }
     }
 
+    // Set default theme
     if (!(themeName || currentTheme || themes[currentTheme] || customThemes[currentTheme]))
       themeName = 'plain';
 
     currentTheme = themeName;
 
+    // If storage is available, store current theme
     if (chrome.storage) {
       chrome.storage.local.set({
         'theme': currentTheme
       });
     }
 
+    // Update theme so it is displayed
     updateTheme();
   }
 
+  /**
+   * Show a dropdown to change the theme
+   */
   function showThemesDropdown() {
+    // Hide the dropdown if it exists
     if (hideDropdown())
       return;
 
+    // Create main dropdown container
     var dropdown = document.createElement('div');
     dropdown.classList.add('dropdown');
     dropdown.innerHTML = '<h2>Themes</h2>';
+    // Show non-saving warning if chrome.storage isn't available
     if (!chrome.storage)
-      dropdown.innerHTML += '<p>Custom themes, and saved settings, are available if you use the Chrome App</p><p>Currently the only way to get it is to clone via GitHub, and add it yourself in the Extensions settings page</p>';
-    dropdown.innerHTML += '<ul></ul>';
-    var themeList = dropdown.querySelector('ul');
+      dropdown.innerHTML += '<p>Saved setting and themes are available if you use the Chrome App</p><p>Currently the only way to get it is to clone via GitHub, and add it yourself in the Extensions settings page</p>';
 
+    // Add a list for themes
+    var themeList = document.createElement('ul');
+    dropdown.appendChild(themeList);
+
+    /**
+     * Add the specified theme to the list of themes to select from
+     */
     function addToDropdownList(key) {
       var theme = themes[key] || customThemes[key];
 
+      // Create item element
       var themeEl = document.createElement('li');
       themeEl.style.backgroundColor = getThemeBackground(key);
       themeEl.style.color = theme.text;
       themeEl.innerHTML = theme.name;
       themeEl.style.fontFamily = getThemeFont(key);
 
+      // Add handler to change theme
       themeEl.addEventListener('click', function() {
         try {
           changeTheme(key);
@@ -166,30 +240,33 @@
       return themeEl;
     }
 
+    // Add default themes
     Object.keys(themes).forEach(function(key) {
       themeList.appendChild(addToDropdownList(key));
     });
+    // Add custom themes, if they're available
     Object.keys(customThemes).forEach(function(key) {
       var themeEl = addToDropdownList(key);
+
+      // Add delete button for custom themes
       themeEl.innerHTML += '<button class="delete"><img src="res/delete.svg" alt="Remove" /></button>';
       var removeButton = themeEl.querySelector('button');
+      // Remove theme if button clicked
       removeButton.addEventListener('click', function() {
         delete customThemes[key];
-        if (chrome.storage) {
+        if (chrome.storage)
           chrome.storage.local.set({
             'customThemes': customThemes
-          }, function() {
-            hideDropdown();
-            showThemesDropdown();
           });
-        } else {
-          hideDropdown();
-          showThemesDropdown();
-        }
+
+        // Hide and re-show dropdown to refresh list
+        hideDropdown();
+        showThemesDropdown();
       });
       themeList.appendChild(themeEl);
     });
 
+    // Add button to create new theme
     var newTheme = document.createElement('li');
     newTheme.innerHTML = 'Create new theme';
     newTheme.addEventListener('click', showNewThemeDropdown);
@@ -198,6 +275,9 @@
     document.querySelector('body').appendChild(dropdown);
   }
 
+  /**
+   * Hide the dropdown if it's there
+   */
   function hideDropdown() {
     var dropdown = document.querySelector('div.dropdown');
     if (dropdown) {
@@ -206,52 +286,64 @@
     }
   }
 
+  /**
+   * Show dropdown for the creation of a new theme
+   */
   function showNewThemeDropdown() {
+    // Hide current dropdown
     hideDropdown();
 
+    // Create dropdown element
     var dropdown = document.createElement('div');
     dropdown.classList.add('dropdown');
     dropdown.innerHTML = '<h2>New Theme</h2>';
-    if (!chrome.storage)
-      dropdown.innerHTML += '<p><strong>Changes made here may have no effect if you\'re using a web browser.</strong></p>';
 
+    // Add warning for lack of chrome.storage
+    if (!chrome.storage)
+      dropdown.innerHTML += '<p><strong>Changes made here won\'t be kept if you\'re using a web browser and close it.</strong></p>';
+
+    // Add CSS colour reference link
     dropdown.innerHTML += '<p>Colors must be in a valid CSS format. Some examples can be found on the <a href="https://developer.mozilla.org/en/docs/Web/CSS/color_value#Color_keywords">Mozilla Developer Network</a></p>';
+    // Add inputs
     dropdown.innerHTML += '<p>Theme name: <input type="text" class="name"></p><p>Text color: <input type="text" class="color"></p><p>Background color: <input type="text" class="bgcolor"></p>';
 
+    // Add list for options
+    var optionList = document.createElement('ul');
+    dropdown.appendChild(optionList);
 
-    dropdown.innerHTML += '<ul></ul>';
-    var optionList = dropdown.querySelector('ul');
-
+    // Add cancel button
     var cancel = document.createElement('li');
     cancel.innerHTML = 'Cancel';
     cancel.addEventListener('click', hideDropdown);
     optionList.appendChild(cancel);
 
+    // Add button to show JSON input
     var byJSON = document.createElement('li');
     byJSON.innerHTML = 'Advanced Input';
     byJSON.addEventListener('click', showJSONDropdown);
     optionList.appendChild(byJSON);
 
+    // Add button to create theme
     var create = document.createElement('li');
     create.innerHTML = 'Create Theme';
     create.addEventListener('click', function() {
+      // Generate randon ID
       var id = Math.floor(Math.random() * 10000);
+      // Set theme
       customThemes[id] = {
         name: dropdown.querySelector('.name').value,
         text: dropdown.querySelector('.color').value,
         background: [dropdown.querySelector('.bgcolor').value]
       };
+      // Add to storage if available
       if (chrome.storage)
         chrome.storage.local.set({
           'customThemes': customThemes
-        }, function() {
-          hideDropdown();
-          changeTheme(id);
         });
-      else {
-        hideDropdown();
-        changeTheme(id);
-      }
+
+      // Hide dropdown and change theme
+      hideDropdown();
+      changeTheme(id);
     });
     optionList.appendChild(create);
 
@@ -259,41 +351,45 @@
   }
 
   function showJSONDropdown() {
+    // Hide current dropdown
     hideDropdown();
 
+    // Create new dropdown container
     var dropdown = document.createElement('div');
     dropdown.classList.add('dropdown');
     dropdown.innerHTML = '<h2>New Theme</h2>';
+    // Add warning for no error checking
     dropdown.innerHTML += '<p>Very little error checking happens here.<br>If you use this input, it is assumed you know what you are doing.</p>';
 
+    // Input JSON input
     dropdown.innerHTML += '<p>JSON:</p><p><input type="text" class="json"></p>';
 
+    // Add list for options
+    var optionList = document.createElement('ul');
+    dropdown.appendChild(optionList);
 
-    dropdown.innerHTML += '<ul></ul>';
-    var optionList = dropdown.querySelector('ul');
-
+    // Add cancel button
     var cancel = document.createElement('li');
     cancel.innerHTML = 'Cancel';
     cancel.addEventListener('click', showNewThemeDropdown);
     optionList.appendChild(cancel);
 
+    // Add creation button
     var create = document.createElement('li');
     create.innerHTML = 'Create Theme';
     create.addEventListener('click', function() {
+      // Pick random ID
       var id = Math.floor(Math.random() * 10000);
       customThemes[id] = JSON.parse(dropdown.querySelector('.json').value);
+      // Store, if storage available
       if (chrome.storage)
         chrome.storage.local.set({
           'customThemes': customThemes
-        }, function() {
-          hideDropdown();
-          changeTheme(id);
         });
-      else {
 
-        hideDropdown();
-        changeTheme(id);
-      }
+      // Change theme
+      hideDropdown();
+      changeTheme(id);
     });
     optionList.appendChild(create);
 
